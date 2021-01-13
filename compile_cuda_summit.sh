b4e84844262972d2b4da6e5838530b3119bfc341
Mathialakan Thavappiragasam <mathialakan@gmail.com>
2021-01-13 14:10:21 -0500

    updates on making ...

--- compile_cuda_summit.sh
+++ compile_cuda_summit.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 
- module load gcc cuda cmake/3.15.2
+ module load gcc/7.4.0 cuda cmake/3.15.2
 
  rm -r CMakeFiles
  rm cmake_install.cmake CPackConfig.cmake CPackSourceConfig.cmake CMakeCache.txt 
- cmake -DWITH_OMP=off
+ cmake 
+ #cmake -DWITH_OMP=off
  make 
 
